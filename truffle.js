311fe2a5765bc670f37e0c6b3a0346ceb74b8d48
Reggie <ryoboua@gmail.com>
2018-08-08 14:58:29 -0400

    Increased Gas Limits

--- truffle.js
+++ truffle.js
@@ -3,7 +3,15 @@ module.exports = {
         development: {
         host: "127.0.0.1",
         port: 7545,
-        network_id: "*" // Match any network id
+        network_id: "*", // Match any network id
+        gas: 4000000,   // <--- Twice as much
+        gasPrice: 10000000000,
+        solc: {
+            optimizer: {
+              enabled: true,
+              runs: 200
+            }
+          }
         }
     }
     };
