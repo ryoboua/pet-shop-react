8babac8d6797c072289ea6b4123a193bdf78b2c3
Reggie <ryoboua@gmail.com>
2018-08-08 14:57:41 -0400

    Added getPetList and CreatPet Function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import getWeb3 from './utils/getWeb3'
 import AdoptionContract from '../build/contracts/Adoption.json'
-import dogList from './pets.json'
 import OwnerComponent from './components/OwnerComponent.js'
 import ClientComponent from './components/ClientComponent.js'
 
@@ -13,6 +12,21 @@ import './App.css'
 const contract = require('truffle-contract')
 const Adoption = contract(AdoptionContract);
 
+const getPetList = async (contractInstance, numOfPets) => {
+  // fetch Pet List and Rebuild Object Array of Pets
+  const promiseArr = [];
+  for (let i = 0; i < numOfPets; i++ ) {
+    promiseArr[i] = await contractInstance.pets(i)
+  }
+    return promiseArr.map( pet => {
+      return {
+        name: pet[0],
+        breed: pet[1],
+        price: pet[2].toString()
+      }
+    })
+}
+
 
 class App extends Component {
   constructor(props) {
@@ -20,16 +34,16 @@ class App extends Component {
 
     this.state = {
       web3: null,
-      dogList: [],
-      adopters: [],
+      petList: [],
       account: null,
       adoptionInstance: null,
       contractOwner: null,
     }
     this.instantiateContract = this.instantiateContract.bind(this)
     this.handleAdopt = this.handleAdopt.bind(this)
-    this.getAccounts = this.getAccounts.bind(this)
+    this.getActiveMetaMaskAccount = this.getActiveMetaMaskAccount.bind(this)
     this.handleReturnPet = this.handleReturnPet.bind(this)
+    this.handleCreatePet = this.handleCreatePet.bind(this)
   }
 
   componentWillMount() {
@@ -51,22 +65,24 @@ class App extends Component {
   }
 
 
-  getAccounts() {
+  getActiveMetaMaskAccount() {
       this.state.web3.eth.getAccounts( (err, accounts) => {
-          console.log(accounts)
           this.setState({ account : accounts[0]})
       })
   }
+
   async instantiateContract() {
     let adoptionInstance;
     Adoption.setProvider(this.state.web3.currentProvider);
-    this.getAccounts()
+    this.getActiveMetaMaskAccount()
+
     adoptionInstance = await Adoption.deployed();
-    let adopters = await adoptionInstance.getAdopters.call()
-    let contractOwner = await adoptionInstance.owner.call()
-    console.log('contractOwner', contractOwner)
-    console.log('Adopter List', adopters)
-    this.setState({ adopters, dogList, adoptionInstance, contractOwner })
+    
+    const contractOwner = await adoptionInstance.owner.call()
+    const TotalNumberOfPets = await adoptionInstance.getTotalNumberOfPets.call().then(x => x.toString())
+    const petList = await getPetList(adoptionInstance, TotalNumberOfPets)
+
+    this.setState({ petList, adoptionInstance, contractOwner })
 
   }
 
@@ -78,41 +94,30 @@ class App extends Component {
     this.state.adoptionInstance.returnPet(index, {from: this.state.account, gas: 4712388, gasPrice: 100000000000})
   }
 
+  handleCreatePet(_name, _breed, _price){
+    this.state.adoptionInstance.createPet(_name, _breed, _price, {from: this.state.contractOwner})
+  }
+
   render() {
     return (
       <div className="App">
         <div style={{textAlign: 'center'}} >
         <h1 style={{margin: 'auto'}}>Adopt a Pet on the Ethereum Network</h1>
         </div>
-        { this.state.account === this.state.contractOwner ?
-          <OwnerComponent /> 
+        { 
+          this.state.account === this.state.contractOwner ?
+          <OwnerComponent
+            petList={this.state.petList}
+            createPet={this.handleCreatePet}
+          /> 
           :
-          <ClientComponent dogList={this.state.dogList} adopters={this.state.adopters} account={this.state.account} />
-        // <Paper style={styles.paperStyle} >
-        //   <GridList cellHeight={160} cols={3} >
-        //     {this.state.dogList.map( (dog, index) => (
-        //           <GridListTile key={index} cols={1}>
-        //             <h2>{ dog.name }</h2>
-        //             <p>{ dog.breed }</p>
-        //             {
-        //             (this.state.adopters[index] !== '0x0000000000000000000000000000000000000000')
-        //             ?
-        //             <Button disabled type="submit" value="Adopt">Pet Adopted</Button>
-        //             :
-        //             <Button color="primary" onClick={() => this.handleAdopt(index)} value="Adopt">Adopt</Button>
-        //             }
-
-        //             {
-        //             (this.state.adopters[index] === this.state.account) ?
-        //                   (<Button color="secondary" type="submit" onClick={ () => this.handleReturnPet(index) } >Return Pet</Button>) 
-        //                   :
-        //                   null
-
-        //             }
-        //           </GridListTile>
-        //   ))}
-        //   </GridList>
-        // </Paper>
+          // <ClientComponent
+          //   dogList={this.state.dogList}
+          //   adopters={this.state.adopters}
+          //   account={this.state.account}
+          // />
+          null
+       
         }
       </div>
     );
