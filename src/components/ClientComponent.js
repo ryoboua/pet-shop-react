2b0810b4c6df050c2c30b9687509106d3a715768
Reggie <ryoboua@gmail.com>
2018-08-10 16:31:28 -0400

    Added event to onClick events

--- src/components/ClientComponent.js
+++ src/components/ClientComponent.js
@@ -3,32 +3,38 @@ import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
-import styles from '../styles.js'
+import styles from '../styles.js';
+import getPetList from '../helpers.js';
+
 
 class ClientComponent extends Component {
     render() {
         return (
             <Paper style={styles.paperStyle} >
-                <GridList cellHeight={160} cols={3} >
-                    {this.props.dogList.map( (dog, index) => (
+                <h1>You are a client</h1>
+                <GridList cellHeight={200} cols={3} >
+                    {this.props.petList.map( (pet, index) => (
                         <GridListTile key={index} cols={1}>
-                            <h2>{ dog.name }</h2>
-                            <p>{ dog.breed }</p>
+                            <h2>{ pet.name }</h2>
+                            <p>{ pet.breed }</p>
+                            <p>{ pet.price }</p>
                             {
-                            (this.props.adopters[index] !== '0x0000000000000000000000000000000000000000')
-                            ?
+                            pet.adopted ?
                             <Button disabled type="submit" value="Adopt">Pet Adopted</Button>
                             :
-                            <Button color="primary" value="Adopt">Adopt</Button>
+                            <Button onClick={() => this.props.adopt(index)} color="primary" value="Adopt">Adopt</Button>
                             }
-
                             {
-                            (this.props.adopters[index] === this.props.account) ?
-                                (<Button color="secondary" type="submit" >Return Pet</Button>) 
+                            (pet.owner === this.props.account) ?
+                                (<Button
+                                  onClick={() => this.props.returnPet(index)} 
+                                  color="secondary" type="submit"
+                                >Return Pet
+                                </Button>) 
                                 :
                                 null
-
                             }
+
                         </GridListTile>
                 ))}
                 </GridList>
--- src/components/OwnerComponent.js
+++ src/components/OwnerComponent.js
@@ -5,11 +5,30 @@ import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import styles from '../styles.js'
 import AddPetFormDialog from './AddPetFormDialog'
+//import getPetList from '../helpers.js';
 
-
+const getPetList = async (contractInstance, numOfPets) => {
+    // fetch Pet List and Rebuild Object Array of Pets
+    const promiseArr = [];
+  
+    if (typeof numOfPets !== 'object') {
+      for (let i = 0; i < numOfPets; i++ ) {
+        promiseArr[i] = await contractInstance.pets(i)
+      }
+    } else {
+  
+    }
+  
+    return promiseArr.map( pet => {
+      return {
+        name: pet[0],
+        breed: pet[1],
+        price: pet[2].toString()
+      }
+    })
+  }
 
 export default class OwnerComponent extends Component {
-
     state = {
         showForm: false,
     };
@@ -26,16 +45,18 @@ export default class OwnerComponent extends Component {
         this.props.createPet(name, breed, price)
     }
     render(){
+
         return (
             <div>
+                <Paper style={styles.paperStyle}>
                 <h1>You are the store Owner</h1>
-                <Paper style={styles.paperStyle} >
-                    <GridList cellHeight={160} cols={3}>
+                    <GridList cellHeight={200} cols={3}>
                         {this.props.petList.map( (pet, index) => (
                             <GridListTile key={index} cols={1}>
                                 <h2>{ pet.name }</h2>
                                 <p>{ pet.breed }</p>
                                 <p>{ pet.price }</p>
+                                <p>Adoption Status: {pet.adopted ? 'True': 'False'}</p>
                             </GridListTile>
                     ))}
                     </GridList>
