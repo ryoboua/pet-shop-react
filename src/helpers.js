14f34d0cb59446122864c76961effec4063786eb
Reggie <ryoboua@gmail.com>
2018-08-17 01:00:04 -0400

    Added fetchPet function

--- src/helpers.js
+++ src/helpers.js
@@ -5,5 +5,16 @@ export const getPetList = async (contractInstance, numOfPets) => {
     for (let i = 0; i < numOfPets; i++ ) 
         { promiseArr[i] = await contractInstance.pets(i) }
 
-    return promiseArr.map( ([ name , breed, price, adopted, owner ]) => ({ name, breed, price: price.toString(), adopted, owner }))
+    return promiseArr.map( ([ name , breed, price, imageURL, adopted, owner ]) => ({ name, breed, price: price.toString(), imageURL, adopted, owner }))
   }
+
+  export const fetchPet = async () => {
+    const newPet = await fetch('https://dog.ceo/api/breeds/image/random').then(result => result.json()).catch(err => console.log(err))
+    if (newPet && newPet.status === 'success') {
+        const breed = newPet.message.split('/')[4]
+        const image = newPet.message
+        return { breed, image  }
+    } else {
+        return null;
+    }
+}
